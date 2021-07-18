474018b9b0d3b052681fc5372e7e55b3ebea0d1a
omnisirus <smirnovceo@yandex.ru>
2021-07-18 23:08:35 +0500

    added: Union th into BasePage class into __init__ method, added MainPage class, and go_to_login_page method into main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,9 @@
+from selenium import webdriver
+from typing import Union
+
+
 class BasePage():
-    def __init__(self, browser, url) -> None:
+    def __init__(self, browser: Union[webdriver.Chrome, webdriver.Firefox], url) -> None:
         self.browser = browser
         self.url = url
 
