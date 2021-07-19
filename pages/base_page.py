d5b46c07421afe8903681d17da8dd54f23d19daa
omnisirus <smirnovceo@yandex.ru>
2021-07-19 14:49:36 +0500

    addedlogin_link var in urls.py module + modified __init__ in with implicitly_wait 5sec and added is_element_present method in BasePage class + added should_be_login_link method in MainPage class + added test_guest_should_see_login_link in main page suite

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,23 @@
+import selenium
+from conftest import browser
 from selenium import webdriver
 from typing import Union
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser: Union[webdriver.Chrome, webdriver.Firefox], url) -> None:
+    def __init__(self, browser: Union[webdriver.Chrome, webdriver.Firefox], url, timeout=5) -> None:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-from .urls import main_page_link
+from .urls import main_page_link, login_link
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +7,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = login_link
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
