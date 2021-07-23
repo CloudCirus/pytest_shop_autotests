d33b98f3217377c7b52b08c416a984741e1b30fb
omnisirus <smirnovceo@yandex.ru>
2021-07-23 17:11:57 +0500

    modifided to_to_login_page with assertion LoginPage obj | refactored test_main_page.py imports

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,16 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_PAGE_LINK)
+        login_link = self.browser.find_element(
+            *MainPageLocators.LOGIN_PAGE_LINK)
         login_link.click()
+        assert LoginPage(
+            self.browser, url=self.browser.current_url), '>>> LOGIN PAGE is not relevant!'
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_PAGE_LINK), '>>> LOGIN LINK is not presented !!! <<<'
+        assert self.is_element_present(
+            *MainPageLocators.LOGIN_PAGE_LINK), '>>> LOGIN LINK is not presented !!! <<<'
--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,20 @@
+from selenium import webdriver
+from typing import Union
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from .urls import main_page_link
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser: Union[webdriver.Chrome, webdriver.Firefox]):
     link = main_page_link
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
+    page.go_to_login_page()
+    # login_page = LoginPage(browser, browser.current_url)
+    # login_page.should_be_login_page()
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser: Union[webdriver.Chrome, webdriver.Firefox]):
     link = main_page_link
     page = MainPage(browser, link)
     page.open()
