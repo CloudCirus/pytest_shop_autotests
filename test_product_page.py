2fb84fde80ae03649dd0300a8c8ce5fa07105b66
omnisirus <smirnovceo@yandex.ru>
2021-07-24 21:56:41 +0500

    added PromoLinks with link generator + marked xfail link in url.py | added test_guest...to_basket_promo with promo links parametrize in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,18 @@
-from typing import Union
-from .urls import MainLinks
+from .urls import MainLinks, PromoLinks
 from .pages.product_page import ProductPage
-
+import pytest
 
 def test_guest_can_add_product_to_basket(browser):
     link = MainLinks.product
+    page = ProductPage(browser, link)
+    page.open()
+    page.can_use_add_to_cart_btn()
+    page.solve_quiz_and_get_code()
+    page.can_add_product_to_basket()
+
+
+@pytest.mark.parametrize('link', PromoLinks.promo_list)
+def test_guest_can_add_product_to_basket_promo(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.can_use_add_to_cart_btn()
--- urls.py
+++ urls.py
@@ -1,4 +1,15 @@
+import pytest
+
+
 class MainLinks:
     main_page = 'http://selenium1py.pythonanywhere.com/'
     login_page = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     product = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+
+class PromoLinks:
+    promo_list = [
+        f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}' \
+        for num in range(10)
+    ]
+    promo_list[7] = pytest.param(promo_list[7], marks=pytest.mark.xfail)
